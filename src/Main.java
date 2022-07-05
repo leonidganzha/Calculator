b4167a74b5ced8d3afc19fe66984641001883e18
leonidganzha <leonidganzha@gmail.com>
2022-07-05 15:03:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,190 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String ans = calc(input);
+        System.out.println(ans);
+    }
+    public static String calc (String input) throws Exception {
+        String[] arr = input.split("\\s+");
+
+        if (arr.length != 3) {
+            throw new Exception("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+        Number num1 = new Number();
+        Number num2 = new Number();
+        num1.literalValue = arr[0];
+        num2.literalValue = arr[2];
+
+        if (num1.isRoman() != num2.isRoman()) {
+            throw new Exception("throws Exception //т.к. используются одновременно разные системы счисления");
+        }
+
+        int a = num1.trueValue();
+        int b = num2.trueValue();
+
+        int res = 0;
+        String operator = arr[1];
+        switch (operator) {
+            case "+":
+                res = a + b;
+                break;
+            case "-":
+                res = a - b;
+                break;
+            case "*":
+                res = a * b;
+                break;
+            case "/":
+                res = a / b;
+                break;
+            default:
+                throw new Exception("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+        String answer = new String();
+        if (num1.isRoman() == true) {
+            if (res < 1) {
+                throw new Exception("throws Exception //т.к. в римской системе нет отрицательных чисел");
+            } else {
+                Number numFin = new Number();
+                answer = numFin.getRomanValue(res);
+            }
+        } else {
+            answer = Integer.toString(res);
+        }
+        return answer;
+    }
+}
+
+class Number {
+    String literalValue;
+
+    boolean isRoman() {
+        switch (literalValue) {
+            case "I":
+                return true;
+            case "II":
+                return true;
+            case "III":
+                return true;
+            case "IV":
+                return true;
+            case "V":
+                return true;
+            case "VI":
+                return true;
+            case "VII":
+                return true;
+            case "VIII":
+                return true;
+            case "IX":
+                return true;
+            case "X":
+                return true;
+            default:
+                return false;
+        }
+    }
+    int trueValue () {
+        switch (literalValue) {
+            case "I":
+                return 1;
+            case "II":
+                return 2;
+            case "III":
+                return 3;
+            case "IV":
+                return 4;
+            case "V":
+                return 5;
+            case "VI":
+                return 6;
+            case "VII":
+                return 7;
+            case "VIII":
+                return 8;
+            case "IX":
+                return 9;
+            case "X":
+                return 10;
+            default:
+                return Integer.parseInt(literalValue);
+            }
+        }
+    String getRomanValue(int num){
+        int firstHalf = num / 10;
+        int secondHalf = num - firstHalf;
+        String firstHalfRoman = new String();
+        String secondHalfRoman = new String();
+        switch (firstHalf) {
+            case 0:
+                firstHalfRoman = "";
+                break;
+            case 1:
+                firstHalfRoman = "X";
+                break;
+            case 2:
+                firstHalfRoman = "XX";
+                break;
+            case 3:
+                firstHalfRoman = "XXX";
+                break;
+            case 4:
+                firstHalfRoman = "XL";
+                break;
+            case 5:
+                firstHalfRoman = "L";
+                break;
+            case 6:
+                firstHalfRoman = "LX";
+                break;
+            case 7:
+                firstHalfRoman = "LXX";
+                break;
+            case 8:
+                firstHalfRoman = "LXXX";
+                break;
+            case 9:
+                firstHalfRoman = "XC";
+                break;
+            case 10:
+                firstHalfRoman = "C";
+                break;
+        }
+        switch (secondHalf) {
+            case 0:
+                secondHalfRoman = "";
+                break;
+            case 1:
+                secondHalfRoman = "I";
+                break;
+            case 2:
+                secondHalfRoman = "II";
+                break;
+            case 3:
+                secondHalfRoman = "III";
+                break;
+            case 4:
+                secondHalfRoman = "IV";
+                break;
+            case 5:
+                secondHalfRoman = "V";
+                break;
+            case 6:
+                secondHalfRoman = "VI";
+                break;
+            case 7:
+                secondHalfRoman = "VII";
+                break;
+            case 8:
+                secondHalfRoman = "VIII";
+                break;
+            case 9:
+                secondHalfRoman = "IX";
+                break;
+        }
+        return firstHalfRoman + secondHalfRoman;
+    }
+}
